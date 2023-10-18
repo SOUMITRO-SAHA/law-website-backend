8c4cb6ee4a0050b3bc99369eef0694900659b348
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-10-18 19:12:45 +0530

    Configuration file updated

--- config/index.js
+++ config/index.js
@@ -10,12 +10,4 @@ exports.config = {
 
   // MONGODB URL:
   MONGODB_URL: process.env.MONGODB_URL,
-
-  // Gmail:
-  GMAIL_USER: process.env.GMAIL_USER,
-  GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
-
-  // Twilio
-  TW_ACCOUNT_SID: process.env.TW_ACCOUNT_SID,
-  TW_AUTH_TOKEN: process.env.TW_AUTH_TOKEN,
 };
